02cadd8a8a9118bf75b818ace145bac9e592566b
nauman_mansuri <mansurinauman7@gmail.com>
2025-05-06 15:22:02 +1000

    added static data for food, beverage (alcholic and non alcoholic)

---
+++ IOS_TASK_3/CategoryData.swift
@@ -0,0 +1,72 @@
+import Foundation
+
+struct CategoryData {
+    static let mealCategories = [
+        "Indian",
+        "South Indian",
+        "North Indian",
+        "Gujarati",
+        "Punjabi",
+        "Rajasthani",
+        "Chinese",
+        "Thai",
+        "Japanese",
+        "Korean",
+        "Mexican",
+        "Italian",
+        "American",
+        "Mediterranean",
+        "Middle Eastern",
+        "Fast Food",
+        "Vegan",
+        "Vegetarian",
+        "BBQ",
+        "Seafood",
+        "Healthy",
+        "Street Food",
+        "African",
+        "European",
+        "Spanish",
+        "Turkish",
+        "French",
+        "Bengali",
+        "Nepali",
+        "Tibetan",
+        "Continental",
+        "Fusion"
+    ]
+
+    static let beverageCategoriesAlcoholic = [
+        "Beer",
+        "Wine",
+        "Cocktail",
+        "Whiskey",
+        "Rum",
+        "Vodka",
+        "Gin",
+        "Tequila",
+        "Brandy",
+        "Sake",
+        "Soju",
+        "Champagne",
+        "Hard Cider"
+    ]
+
+    static let beverageCategoriesNonAlcoholic = [
+        "Tea",
+        "Coffee",
+        "Juice",
+        "Soda",
+        "Milkshake",
+        "Smoothie",
+        "Mocktail",
+        "Lassi",
+        "Coconut Water",
+        "Energy Drink",
+        "Lemonade",
+        "Buttermilk",
+        "Flavored Water",
+        "Herbal Tea",
+        "Hot Chocolate"
+    ]
+}
